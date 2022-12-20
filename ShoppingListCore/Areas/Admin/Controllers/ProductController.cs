558286682aeb7e32729b4c925e737b6ffc41c252
gulelif <elifgul12218@gmail.com>
2022-12-20 13:57:08 +0300

    Add project files.

---
+++ ShoppingListCore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,208 @@
+﻿
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ShoppingListCore.Repository;
+using ShoppingListCore.Validators;
+using ShoppingListCoreProject.Models;
+using ShoppingListProject.Models;
+using ShoppingListProject.Validators;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ShoppingListCore.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]")]
+    public class ProductController : Controller
+    {
+       
+        GenericRepository<Product> productRepository = new GenericRepository<Product>();
+        GenericRepository<Category> categoryRepository = new GenericRepository<Category>();
+                
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            GetCategories(0,1);   //    Öncelikli id yok "Tümü" seçeneği aktif
+           var  product = productRepository.GetAll(null,x=>x.Category);
+            return View(product);
+        }
+        public IActionResult FilterProduct(int id)
+        {
+            //Kategoriye göre filtreleme
+     
+            var productlist = productRepository.GetAll(null, x=>x.Category);
+
+            if (id != 0)
+            {
+                productlist = productlist.Where(c => c.CategoryId == id).ToList();
+            }
+
+            GetCategories(id,1);
+
+         
+            return View("Index", productlist);
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+           GetCategories();
+           
+           
+            return View();
+
+        }
+        private void GetCategories(int id = 0,int isfilter=0)
+        {
+            //kategori listesini getirir
+            List<Category> categories = new List<Category>();
+
+            var categories1 = categoryRepository.GetList();
+            if (id != 0)
+            {
+                //ürün sayfasına gönderirken dropdownda ilk sırada filtrelenen ürünün kategorisi olsun
+                var ct = categoryRepository.GetByID(id);
+                categories.Add(ct);
+                if (isfilter == 1)//ürün ekleme ve güncelleme sayfasından 0 gelir ve tümü seçeneği olmaz
+                    categories.Add(new Category { CategoryId = 0, CategoryName = "Tümü" });
+                foreach (var item in categories1)
+                {
+                    if (item.CategoryId != ct.CategoryId)
+                    {
+                        categories.Add(item);
+                    }
+
+                }
+
+
+
+            }
+            else
+            {
+                if(isfilter==1)//ürün ekleme ve güncelleme sayfasından 0 gelir ve tümü seçeneği olmaz
+                categories.Add(new Category { CategoryId = 0, CategoryName = "Tümü" });
+                foreach (var item in categories1)
+                {
+
+                    categories.Add(item);
+
+
+                }
+            }
+            ViewBag.Categories = new SelectList(categories, "CategoryId", "CategoryName");
+       
+           
+        }
+        [HttpPost]
+        public  async Task<IActionResult> AddProduct(Product product)
+        {
+            GetCategories();
+
+            ProductValidator validator = new ProductValidator();
+
+            ValidationResult result = validator.Validate(product);
+            if (result.IsValid == false)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+                return View();
+
+            }
+
+           
+
+                product.CategoryId = product.Category.CategoryId;
+                product.Category = null;
+              //ürün adı daha önce kullanılmamışsa ekler
+            var controll = productRepository.GetListByFilter(s => s.ProductName.ToLower() == product.ProductName.ToLower());
+            if (controll.Count ==0)
+            {
+
+                productRepository.Insert(product);
+                ViewBag.message = "Kayıt Başarılı!";
+            }
+            else
+            {
+                 ViewBag.message = "Ürün daha önce eklenmiş!";
+            }
+                                 
+
+            return View();
+
+
+
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            productRepository.Delete(productRepository.GetByID(id));
+            return RedirectToAction("Index", "Product", new { area = "Admin" });
+
+        }
+
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            //güncellenecek ürünün bilgileri getirilir
+            var ch = productRepository.GetByID(id);
+            GetCategories(ch.CategoryId);
+          
+            return View(ch);
+
+
+        }
+        [HttpPost]
+        public IActionResult UpdateProduct(Product  product)
+        { 
+           
+            GetCategories(product.CategoryId);
+            ProductValidator validator = new ProductValidator();
+
+            ValidationResult result = validator.Validate(product);
+            if (result.IsValid == false)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+               
+                return View(product);
+
+            }
+          
+                product.CategoryId = product.Category.CategoryId;
+                product.Category = null;
+               if(product.Image==null)//yeni image seçilmediyse eskisini al
+            {
+             product.Image= productRepository.GetByID(product.ProductId).Image;
+
+            }
+
+            var controll = productRepository.GetByFilter(s => s.ProductName.ToLower() == product.ProductName.ToLower());
+          
+            if (controll == null || controll.ProductId==product.ProductId  )
+            {
+                productRepository.Update(product);             
+                ViewBag.message = "Kayıt Başarılı!";
+            }
+            else
+            {
+                ViewBag.message = "Ürün daha önce eklenmiş!";
+            }
+            return View(product);
+           
+        }
+    
+
+}
+}
---
+++ ShoppingListCore/Controllers/LoginController.cs
@@ -0,0 +1,170 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+
+using ShoppingListCore.Models;
+using ShoppingListCore.Repository;
+using ShoppingListCoreProject.Models;
+using ShoppingListProject.Models;
+using ShoppingListProject.Validators;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace ShoppingListCoreProject.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        //User tablosu için repository oluşturuyoruz
+        GenericRepository<User> userRepository = new GenericRepository<User>();
+
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            HttpContext.SignOutAsync();                   
+            return View();
+        }
+        [HttpPost]
+        public  IActionResult SignUp(UserRegisterViewModel u) 
+        {
+            //UserRegisterViewModel içerisinde User bilgileri ve confirmpassword var 
+            //bilgilerin uservalidatordaki şartları sağlayıp sağlamadığına bakıyoruz
+            UserValidator validator = new UserValidator();
+            ValidationResult result = validator.Validate(u);
+            if (result.IsValid == false)
+            {
+                foreach (var error in result.Errors)
+                {
+                    //şart sağlanmamışsa ekrana hata mesajını gönderiyoruz
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+                return View();
+
+            }
+          
+            User user = new User()
+            {
+                UserPassword = u.UserPassword,
+                UserSurname = u.UserSurname,
+                UserMail = u.UserMail,
+                UserName = u.UserName
+            };
+
+           
+           
+                //veritabanında 
+                var mail = userRepository.GetByFilter(x => x.UserMail == user.UserMail);
+            if (mail == null)//veritabanında böyle bir mail yoksa kaydet
+            {
+                userRepository.Insert(user);
+                return RedirectToAction("SignIn");
+            }
+
+           
+            else
+            {
+                ModelState.AddModelError("emailused", "Mail adresi daha önce alınmış");
+                return View();
+            }
+
+
+        }
+
+        [HttpGet]
+        public  IActionResult SignIn()
+        {
+            //Daha önce giriş yapılmışsa SignIn sayfasından rolüne göre yönlendiriyoruz.
+         if( User.IsInRole("Administrator"))
+            { return RedirectToAction("Index", "Category", new { area = "Admin" });}  
+
+         if(User.IsInRole("Member"))
+            {  return RedirectToAction("Index", "List", new { area = "Member" }); }
+            //Giriş yapılmamışsa SignIn sayfasına yönlendiriyoruz
+            var Userid = String.IsNullOrEmpty(HttpContext.User.Identity.Name);
+
+            if (Userid)
+                ViewBag.isauthenticated = false;
+            else
+                ViewBag.isauthenticated = true;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInViewModel p)
+        {
+            //Sayfadan gelen kullanıcı adı ve parolanın boş olup olmadığını kontrol ediyoruz
+            LoginValidator validator = new LoginValidator();
+            ValidationResult result = validator.Validate(p);
+            if (!result.IsValid)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                }
+                return View();
+
+            }
+            //boş değilse..
+            if (result.IsValid)
+            {
+                string role = "";
+                User user=ControlLogin(p);
+
+             
+                if(user!=null)
+                {
+                    //Kullanıcı rolünü belirledik
+                    role=user.UserAdminStatus ?  "Administrator" : "Member";
+                 
+                    //Authentication işlemlerini cookie ile yapıyoruz
+                    var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserId.ToString()),
+                    new Claim(ClaimTypes.Role, role),
+                };
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var authProperties = new AuthenticationProperties();
+                   
+                    await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);//önceden açık oturum varsa kapat
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity),
+                    authProperties);//oturumu aç
+
+                    //kullanıcı rolüne göre yönlendirme işlemi
+                    return user.UserAdminStatus 
+                        ? RedirectToAction("Index", "Category", new { area = "Admin" }) 
+                        : RedirectToAction("Index", "List", new { area = "Member" });
+                  
+
+                }
+
+                else
+            {
+                ViewBag.message = "Hatalı Kullanıcı adı veya Parola";
+            }
+                }
+          
+
+            return View();
+           
+        }
+        private User ControlLogin(UserSignInViewModel p)
+        {
+           //mail adresi ve parolanın veritabanından kontrolü
+            var user = userRepository.GetByFilter(x => x.UserMail == p.email && x.UserPassword == p.password);
+            return user;
+
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            //oturumu kapat
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
